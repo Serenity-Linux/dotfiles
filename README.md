b207a428deb35b119b3bd1b827bcb1a3e9dc6055
Serenity-Linux <arnaud.maussang@gmail.com>
2025-05-19 00:11:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+@Serenity-Linux's Hyprland Rice on Arch Linux
+
+Prerequisites: Please install ArchLinux and Hyprland first using the archinstall script in the ArchLinux ISO image. Please backup your existing dotfiles!!!
+
+Installation: Once done, you can git clone the repository and execute the script 'install.sh'.
+
+Post installation:
+
+if using a mac with azerty-mac keyboard layout, run this command: $ localectl set-x11-keymap fr mac-fr
+
+Spicetify: Launch spotify then run these commands: $ sudo chmod a+wr /opt/spotify $ sudo chmod a+wr /opt/spotify/Apps -R $ spicetify backup apply $ spicetify config current_theme Comfy $ spicetify apply
+
+NvChad: ##Run :MasonInstallAll command after lazy.nvim finishes downloading plugins. ##Delete the .git folder from nvim folder. ##Learn customization of ui & base46 from :h nvui
+
+Pywalfox: Launch Firefox and download the extension Pywalfox
+
+Last step, enjoy!
+
---
+++ install.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+clear
+
+######################################
+#####Git Repositories & Paru##########
+######################################
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ../
+
+######################################
+########Essential packages############
+######################################
+sudo pacman -Syyu
+paru -S --needed nvim zsh iwd git firefox hyprpaper swww viewnior ninja gcc wayland-protocols libjpeg-turbo libwebp libjxl pango cairo pkgconf cmake libglvnd wayland hyprutils hyprwayland-scanner hyprlang brightnessctl pavucontrol playerctl base-devel wget code waypaper wlogout python-pywal16 python-watchdog python-pywalfox matugen-bin glib2-devel hyprlock waybar sddm bluez bluez-utils nautilus swaync alacritty cava fastfecth rofi
+
+######################################
+#######Oh-my-zsh & plugins############
+######################################
+
+###Oh-my-zsh###
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+###zsh-autosuggestions###
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+###syntax highligthing###
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+###syntax faster highligthing###
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlightingpqru 
+
+######################################
+###iwd backend for network manager####
+######################################
+sudo cp NetworkManager.conf /etc/NetworkManager
+
+#replace networkManager backend from wpa_supplicant to iwd
+#etc/NetworkManager/NetworkManager.conf -> add
+#device between square bracket
+#wifi.backend=iwd
+#wifi.iwd.autoconnect=yes
+
+sudo systemctl start iwd; sudo systemctl enable iwd
+sudo systemctl stop wpa_supplicant; sudo systemctl disable wpa_supplicant
+
+######################################
+#############Bluetooth################
+######################################
+systemctl enable bluetooth.service
+
+#for macbook pro 13 2015
+#localectl set-x11-keymap fr mac-fr
+
+
+######################################
+##############Fonts###################
+######################################
+sudo cp -R dm-sans /usr/share/fonts
+sudo cp -R radio-canada-big /usr/share/fonts
+
+######################################
+#############Pywalfox#################
+######################################
+sudo pywalfox install
+
+######################################
+#############NvChad###################
+######################################
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+
+######################################
+#############chadwal##################
+######################################
+git clone https://github.com/NvChad/pywal ~/.config/nvim
+
+######################################
+###############sddm###################
+######################################
+git clone https://codeberg.org/minMelody/sddm-sequoia.git ~/sequoia && rm -rf ~/sequoia/.git
+sudo mv ~/sequoia /usr/share/sddm/themes/
+sudo mv sddm.conf /etc/sddm.conf.d/
+
+######################################
+###############ROFI###################
+######################################
+cp -r material-you $HOME/.local/share/rofi/themes
+
+######################################
+############dotfiles##################
+######################################
+cp -r .config $HOME
+cp -r Wallpapers $HOME
+cp .zshrc $HOME
+
+######################################
+############scripts###################
+######################################
+chmod +x $HOME/.config/hypr/const/terminal.sh
+chmod +x $HOME/.config/hypr/const/browser.sh
+chmod +x $HOME/.config/hypr/const/menu.sh
+chmod +x $HOME/.config/hypr/const/filemanager.sh
+chmod +x $HOME/.config/hypr/scripts/switch-keyboard-layout.sh
+chmod +x $HOME/.config/hypr/scripts/keyboard-identifier.sh
